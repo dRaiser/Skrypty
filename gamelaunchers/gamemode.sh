949bfeb1643e853aa6ec579e1c09d791af1d80ea
dRaiser <pw@draiser.net>
2013-07-03 21:01:06 +0200

    1st commit

---
+++ gamelaunchers/gamemode.sh
@@ -0,0 +1,17 @@
+if [ $1 == 1 ]; then
+	pkill geary
+	pkill owncloud
+	rm /home/draiser/.local/share/data/ownCloud/lock
+	gsettings set org.cinnamon panel-autohide true
+	sudo cpupower frequency-set -g performance
+	export SDL_VIDEO_FULLSCREEN_DISPLAY=0
+elif [ $1 == 0 ]; then
+	geary &
+	turnonleftmonitor
+	sleep 2
+	devilspie -a > /dev/null 2>&1 &
+	sh /home/draiser/Skrypty/restart_conky.sh > /dev/null 2>&1 &
+	gsettings set org.cinnamon panel-autohide false
+	sudo cpupower frequency-set -g ondemand
+	xrandr --output DVI-I-2 --mode 1920x1080
+fi
